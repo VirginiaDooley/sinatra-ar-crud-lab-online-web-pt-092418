ae120bcd37378706762a524968e5c8bb5acab5e9
Virginia Dooley <vcdooley@gmail.com>
2019-01-16 17:24:18 +0000

    saving changes, still working on a solution to Create function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,39 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  #Create
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    # binding.pry
+
+    @article = Article.create(params)
+    # @article1 = Article.create(:title => article_title, :content => article_content)
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #Read (general)
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  #Read (specific)
+  get 'articles/:id' do
+    #creates an instance variable to pass on data to a view
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #Update
+  get 'articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
 end
